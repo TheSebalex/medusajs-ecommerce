e0f727111590d36af20d92c7aa74f08b240908af
Sebastian Leanez <thesebalex111@gmail.com>
2024-06-23 16:37:54 -0400

    ready

--- glamora/src/admin/components/shared/accordion.tsx
+++ glamora/src/admin/components/shared/accordion.tsx
@@ -25,65 +25,67 @@ const Item: any = ({
   active,
   triggerable,
   ...props
-}): any => {
+} : any): any => {
   return (
-    <AccordionPrimitive.Item
-      {...props}
-      value={title}
-      className={clx(
-        "border-grey-20 group border-t last:mb-0",
-        "py-1 px-8",
-        className
-      )}
-    >
-      <AccordionPrimitive.Header className="px-1">
-        <div className="flex flex-col">
-          <div className="flex w-full items-center justify-between">
-            <div className="flex items-center gap-4">
-              <div className="flex items-center justify-center p-[10px]">
-                {complete ? (
-                  <CheckCircleSolid className="text-ui-fg-interactive" />
-                ) : (
-                  <>
-                    {active && (
-                      <ActiveCircleDottedLine
-                        size={20}
-                        className="text-ui-fg-interactive rounded-full"
-                      />
-                    )}
-                    {!active && (
-                      <CircleMiniSolid className="text-ui-fg-muted" />
-                    )}
-                  </>
-                )}
-              </div>
-              <Heading level="h3" className={clx("text-ui-fg-base")}>
-                {title}
-              </Heading>
-            </div>
-            <AccordionPrimitive.Trigger>
-              {customTrigger || <MorphingTrigger />}
-            </AccordionPrimitive.Trigger>
-          </div>
-          {subtitle && (
-            <Text as="span" size="small" className="mt-1">
-              {subtitle}
-            </Text>
-          )}
-        </div>
-      </AccordionPrimitive.Header>
-      <AccordionPrimitive.Content
-        forceMount={forceMountContent}
+    <>
+      <AccordionPrimitive.Item
+        {...props}
+        value={title}
         className={clx(
-          "radix-state-closed:animate-accordion-close radix-state-open:animate-accordion-open radix-state-closed:pointer-events-none px-1"
+          "border-grey-20 group border-t last:mb-0",
+          "py-1 px-8",
+          className
         )}
       >
-        <div className="inter-base-regular group-radix-state-closed:animate-accordion-close">
-          {description && <Text>{description}</Text>}
-          <div className="w-full">{children}</div>
-        </div>
-      </AccordionPrimitive.Content>
-    </AccordionPrimitive.Item>
+        <AccordionPrimitive.Header className="px-1">
+          <div className="flex flex-col">
+            <div className="flex w-full items-center justify-between">
+              <div className="flex items-center gap-4">
+                <div className="flex items-center justify-center p-[10px]">
+                  {complete ? (
+                    <CheckCircleSolid className="text-ui-fg-interactive" />
+                  ) : (
+                    <>
+                      {active && (
+                        <ActiveCircleDottedLine
+                          size={20}
+                          className="text-ui-fg-interactive rounded-full"
+                        />
+                      )}
+                      {!active && (
+                        <CircleMiniSolid className="text-ui-fg-muted" />
+                      )}
+                    </>
+                  )}
+                </div>
+                <Heading level="h3" className={clx("text-ui-fg-base")}>
+                  {title}
+                </Heading>
+              </div>
+              <AccordionPrimitive.Trigger>
+                {customTrigger || <MorphingTrigger />}
+              </AccordionPrimitive.Trigger>
+            </div>
+            {subtitle && (
+              <Text as="span" size="small" className="mt-1">
+                {subtitle}
+              </Text>
+            )}
+          </div>
+        </AccordionPrimitive.Header>
+        <AccordionPrimitive.Content
+          forceMount={forceMountContent}
+          className={clx(
+            "radix-state-closed:animate-accordion-close radix-state-open:animate-accordion-open radix-state-closed:pointer-events-none px-1"
+          )}
+        >
+          <div className="inter-base-regular group-radix-state-closed:animate-accordion-close">
+            {description && <Text>{description}</Text>}
+            <div className="w-full">{children}</div>
+          </div>
+        </AccordionPrimitive.Content>
+      </AccordionPrimitive.Item>
+    </>
   );
 };
 
